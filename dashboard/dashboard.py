41a28f192f9d7dfa0600dffa20404e3ca45e2a6c
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-06-29 23:20:58 -0400

    Created a significantly prettier set of charts

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,7 @@ import datetime
 import dash
 import pandas as pd
 import plotly.express as px
-from dash import html, dcc
+from dash import html, dcc, Input, Output
 
 # App initialization
 app = dash.Dash(
@@ -19,29 +19,94 @@ df["Date"] = pd.to_datetime(df["Date"])
 df["Volume"] = df["Weight"] * df["Total Reps"]
 df["Projected 1RM"] = df["Weight"] * (1 + (df["Reps"] / 30))
 
-# Create plot
-fig = px.line(df, x="Date", y="Volume", color="Exercise", facet_col="Muscle Groups", facet_col_wrap=2, title="Lift Volume by Exercise Over Time")
-fig2 = px.line(df, x="Date", y="Projected 1RM", color="Exercise", facet_col="Muscle Groups", facet_col_wrap=2, title="Projected 1RM by Exercise Over Time")
-last_three_months = df[df["Date"] >= datetime.date.today() - pd.offsets.MonthBegin(3)]
-fig3 = px.line(last_three_months, x="Date", y="Volume", color="Exercise", facet_col="Muscle Groups", facet_col_wrap=2, title="Lift Volume by Exercise Over Last Three Months")
-fig4 = px.line(last_three_months, x="Date", y="Projected 1RM", color="Exercise", facet_col="Muscle Groups", facet_col_wrap=2, title="Projected 1RM by Exercise Over Last Three Months")
-
 # App layout
 app.layout = html.Div([
-    html.H1("Dashboard Template"),
+    html.H1("Health Dashboard"),
     html.P(
         """
-        Welcome to The Renegade Coder template for Dash apps!
-        Place your HTML here to create your own dashboard. 
-        Make use of the graphs component of dcc to load Plotly
-        graphs. 
+        Welcome to my Health Dashboard. I use this space to track my health and fitness progress
+        using pretty pictures. Use the dropdown to pick a window of time besides "All Time".
         """
     ),
-    dcc.Graph(className="custom-height", figure=fig),
-    dcc.Graph(className="custom-height", figure=fig2),
-    dcc.Graph(className="custom-height", figure=fig3),
-    dcc.Graph(className="custom-height", figure=fig4)
+    dcc.Dropdown(["All Time", "Last Three Months"], "All Time", id="dropdown"),
+    html.H2("Lift Volume"),
+    html.P(
+      """
+      For the sake of tracking, I define lift volume as the weight of the lift multiplied by 
+      the number of total reps across all sets. Volumes are computed for all exercises and
+      are grouped by muscle below. 
+      """
+    ),
+    dcc.Graph(id="exercise-volume-over-time", className="custom-height"),
+    html.H2("Projected One Rep Maximum"),
+    html.P(
+      """
+      Projected 1RM is computed by using the standard 1RM formula to
+      determine the maximum amount of weight you could probably lift. 
+      This is a more useful metric for tracking progress than volume
+      because it gives you a better idea how how you're building muscle.
+      Plots are still somewhat erratic because I don't always lift to
+      failure. 
+      """
+    ),
+    dcc.Graph(id="1rm-over-time", className="custom-height")
 ])
+
+@app.callback(
+  Output("exercise-volume-over-time", "figure"),
+  Input("dropdown", "value")
+)
+def update_exercise_volume(dropdown_value):
+  display_order = {"Muscle Groups": sorted(df["Muscle Groups"].unique()), "Exercise": sorted(df["Exercise"].unique())}
+  if dropdown_value == "Last Three Months":
+    last_three_months = df[df["Date"] >= datetime.date.today() - pd.offsets.MonthBegin(3)]
+    return px.line(
+      last_three_months, 
+      x="Date", 
+      y="Volume", 
+      color="Exercise", 
+      facet_col="Muscle Groups", 
+      facet_col_wrap=2, 
+      title="Lift Volume by Muscle Group: Last Three Months",
+      category_orders=display_order
+    )
+  return px.line(
+    df, 
+    x="Date", 
+    y="Volume", 
+    color="Exercise", 
+    facet_col="Muscle Groups", 
+    facet_col_wrap=2, 
+    title="Lift Volume by Muscle Group: All Time",
+    category_orders=display_order
+  )
+
+@app.callback(
+  Output("1rm-over-time", "figure"),
+  Input("dropdown", "value")
+)
+def update_1rm(dropdown_value):
+  if dropdown_value == "Last Three Months":
+    last_three_months = df[df["Date"] >= datetime.date.today() - pd.offsets.MonthBegin(3)]
+    return px.line(
+      last_three_months, 
+      x="Date", 
+      y="Projected 1RM", 
+      color="Exercise", 
+      facet_col="Muscle Groups", 
+      facet_col_wrap=2, 
+      title="Projected 1RM by Muscle Group: Last Three Months"
+    )
+  return px.line(
+    df, 
+    x="Date", 
+    y="Projected 1RM", 
+    color="Exercise", 
+    facet_col="Muscle Groups", 
+    facet_col_wrap=2, 
+    title="Projected 1RM by Muscle Group: All Time"
+  )
+
 
 if __name__ == '__main__':
   app.run_server(debug=True)
