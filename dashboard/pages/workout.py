d0a83a07aa8cfc413b6f0a73a8171fff1f87ab3a
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-07-06 15:56:02 -0400

    Cleaned up workout code

--- dashboard/pages/workout.py
+++ dashboard/pages/workout.py
@@ -3,6 +3,21 @@ import dash
 from dash import html, dcc, callback, Input, Output
 import pandas as pd
 import plotly.express as px
+import dash_bootstrap_components as dbc
+
+
+descriptions = {
+  "Back":
+    """
+    To me, the back is a generic muscle group mainly referring to the traps. 
+    """,
+  "Dumbbell Rows":
+    """
+    Dumbell rows are traditional rows that are performed bent over using a single arm at a time,
+    at least that's how I do them. In short, I usually just bend down with one hand on a
+    surface and the other pulling the weight toward me from the ground.
+    """,
+}
 
 
 dash.register_page(__name__)
@@ -18,62 +33,105 @@ fatigue = df[df["Date"] >= datetime.date.today() - pd.offsets.Day(2)] \
   .groupby("Muscle Groups") \
   .agg({"Volume": "sum", "Projected 1RM": "mean"})
 missing = set(df["Muscle Groups"].unique()) - set(fatigue.index)
-fatigue["Cumulative Volume / Average Project 1RM"] = fatigue["Volume"] / fatigue["Projected 1RM"]
+fatigue["Cumulative Volume / Average Project 1RM"] = fatigue["Volume"] / \
+  fatigue["Projected 1RM"]
 for muscle in missing:
   fatigue.loc[muscle] = 0
-fatigue = fatigue.sort_values("Cumulative Volume / Average Project 1RM", ascending=True)
+fatigue = fatigue.sort_values(
+  "Cumulative Volume / Average Project 1RM", 
+  ascending=True
+)
 fig = px.bar(fatigue, y="Cumulative Volume / Average Project 1RM")
 
 # Setup layout
 layout = html.Div([
-    html.H2("Muscle Fatigue"),
-    html.P(
-      """
-      Muscle fatigue is a metric that I use to track how much muscle I've used in the
-      past 48 hours. It's a bit rudimentary, but basically I compute a ratio of muscle 
-      group volume (sum) against the projected 1RM (mean). This is a pretty sloppy metric 
-      since I don't find 1RM very accurate and the aggregate functions are somewhat misleading, 
-      but I don't really think it needs to be that accurate. Regardless, it's not like I have
-      a scientific way of assessing fatique anyway.
-      """
-    ),
-    dcc.Graph(figure=fig),
-    html.H2("Exercise Sets and Reps"),
-    html.P(
-      """
-      This last section is just bookkeeping for me. It's hard to remember how much weight I did
-      last, so I made plots of the individual exercise by set and rep.
-      """
-    ),
-    html.Div(id="exercise-sets-reps"),
+  html.H2("Muscle Fatigue"),
+  html.P(
+    """
+    Muscle fatigue is a metric that I use to track how much muscle I've used in the
+    past 48 hours. It's a bit rudimentary, but basically I compute a ratio of muscle 
+    group volume (sum) against the projected 1RM (mean). This is a pretty sloppy metric 
+    since I don't find 1RM very accurate and the aggregate functions are somewhat misleading, 
+    but I don't really think it needs to be that accurate. Regardless, it's not like I have
+    a scientific way of assessing fatique anyway.
+    """
+  ),
+  dcc.Graph(figure=fig),
+  html.H2("Exercise Sets and Reps"),
+  html.P(
+    """
+    This last section is just bookkeeping for me. It's hard to remember how much weight I did
+    last, so I made plots of the individual exercise by set and rep.
+    """
+  ),
+  html.Div(id="exercise-sets-reps"),
 ])
+
+
+def time_filter(df: pd.DataFrame, window: str):
+  """
+  A help function to filter the dataframe by time window.
+  """
+  curr = df
+  if window == "Last Three Months":
+    curr = df[df["Date"] >= datetime.date.today() - pd.offsets.MonthBegin(3)]
+  return curr
+
+
+def plot_exercise_sets_reps(df: pd.DataFrame, window: str, muscle: str, exercise: str):
+  """
+  :param df: the complete dataset
+  :param window: the time window to filter by
+  :param muscle: the muscle group to filter by
+  :param exercise: the exercise to filter by
+  """
+  temp = time_filter(df, window)
+  muscle_df = temp[temp["Muscle Groups"] == muscle]
+  exercise_df = muscle_df[muscle_df["Exercise"] == exercise]
+  figure = px.line(
+    exercise_df,
+    x="Date",
+    y="Weight",
+    facet_col="Sets",
+    color="Reps",
+    category_orders={
+      # Ensures only existing sets are shown
+      "Sets": sorted(exercise_df["Sets"].unique()),
+      # Ensures colors are constant between plots
+      "Reps": sorted(df["Reps"].unique())
+    },
+    markers=True
+  )
+  return figure
+
 
 @callback(
   Output("exercise-sets-reps", "children"),
   Input("dropdown", "value")
 )
 def update_exercise_sets_reps(dropdown_value):
-  exercise_plots = []
-  curr = df
-  if dropdown_value == "Last Three Months":
-    curr = df[df["Date"] >= datetime.date.today() - pd.offsets.MonthBegin(3)]
+  children = []
+  curr = time_filter(df, dropdown_value)
   for muscle in sorted(curr["Muscle Groups"].unique()):
-    exercise_plots.append(html.H3(muscle))
+    children.append(html.H3(muscle))
     curr_muscle = curr[curr["Muscle Groups"] == muscle]
     for exercise in sorted(curr_muscle["Exercise"].unique()):
       curr_exercise = curr_muscle[curr_muscle["Exercise"] == exercise]
-      figure = px.line(
-        curr_exercise, 
-        x="Date", 
-        y="Weight", 
-        facet_col="Sets", 
-        color="Reps",
-        title=exercise,
-        category_orders={
-          "Sets": sorted(curr_exercise["Sets"].unique()), # Ensures only existing sets are shown
-          "Reps": sorted(df["Reps"].unique()) # Ensures colors are constant between plots
-        },
-        markers=True
-      )
-      exercise_plots.append(dcc.Graph(figure=figure))
-  return exercise_plots
+      children.append(html.H4(exercise))
+      children.append(descriptions.get(exercise, ""))
+      difficulty = curr_exercise["Difficulty"].iloc[-1]
+      sets = curr_exercise["Sets"].iloc[-1]
+      reps = curr_exercise["Reps"].iloc[-1]
+      weight = curr_exercise["Weight"].iloc[-1]
+      children.append(html.P(
+        [
+          f"Last time I did a set (",
+          html.B(f"{sets}x{reps}x{weight}"),
+          "), I described it as ",
+          html.B(f"{difficulty}"),
+          "."
+        ]
+      ))
+      figure = plot_exercise_sets_reps(df, dropdown_value, muscle, exercise)
+      children.append(dcc.Graph(figure=figure))
+  return children
